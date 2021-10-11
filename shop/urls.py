1b96d5ec0ab7126bbd453a97c694e3e880061563
gvilkson <aisten@vsoft.net.br>
2021-10-10 20:49:33 -0400

    Criando pagina carrinho

--- shop/urls.py
+++ shop/urls.py
@@ -7,4 +7,5 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', v.shop, name='shop'),
+    path('cart/', v.cart, name='cart'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # @login_required
 def shop(request):
     return render(request, 'shop.html', {})
+
+def cart(request):
+    return render(request, 'cart.html', {})
